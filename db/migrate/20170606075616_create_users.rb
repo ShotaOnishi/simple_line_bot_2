90178304843d4f2d6cf92a3e6d5a850bcf634c23
ShotaOnishi <showta.0804@gmail.com>
2017-06-06 09:37:49 +0000

    a

--- db/migrate/20170606075616_create_users.rb
+++ db/migrate/20170606075616_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :line_id, unique: true
+      t.string :line_id
       t.timestamps null: false
     end
   end
