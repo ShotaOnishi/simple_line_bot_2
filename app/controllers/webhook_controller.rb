9e9d45e912c346ddcb19af9db08652ce8056d75e
ShotaOnishi <showta.0804@gmail.com>
2017-06-08 14:57:29 +0000

    c

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -17,10 +17,9 @@ class WebhookController < ApplicationController
 
     case event_type
     when "message"
-      #user = User.find_or_create_by(:line_id => event["source"]["userId"])
-      #chat = Message.create(:chat_text => event["message"]["text"], :user_id => user.id)
-      #output_text = chat.chat_text
-      output_text = "hello, world"
+      user = User.find_or_create_by(:line_id => event["source"]["userId"])
+      chat = Message.create(:chat_text => event["message"]["text"], :user_id => user.id)
+      output_text = chat.chat_text
       #output_text = event["source"]["userId"]
       #output_text = input_text
     end
